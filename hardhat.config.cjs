f9a6ce1d15e2705afe95f2ef3b1b3ecdfc073255
bong874vu <bong874vu@gmail.com>
2026-06-08 15:00:03 +0700

    chore(config): upgrade solidity compiler and configure hardhat-ethers plugin

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,10 +1,12 @@
 require("dotenv").config();
-require("dotenv").config();
+const hardhatEthers = require("@nomicfoundation/hardhat-ethers").default || require("@nomicfoundation/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.28",
+  plugins: [hardhatEthers],
   networks: {
     arc: {
+      type: "http",
       url: "https://rpc.testnet.arc.network",
       chainId: 5042002,
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
